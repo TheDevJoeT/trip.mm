c4e9a4d0abd7e4b0c28e545a187fb7faec265bd3
JoeT1327 <thantziaung.joe631@gmail.com>
2026-02-13 10:08:02 +0630

    epi 68 understanding useEffect dependencies and applying on buttons

--- src/components/TripList/index.js
+++ src/components/TripList/index.js
@@ -2,23 +2,24 @@ import React, { useEffect, useState } from "react";
 
 export default function TripList() {
   let [trips, setTrips] = useState([]);
+  let [url, setUrl] = useState("http://localhost:3010/trips");
   
 
   useEffect(() => {
-    fetch("http://localhost:3010/trips")
+    fetch(url)
       .then((res) => res.json())
       .then((data) => {
         setTrips(data);
         // never use async/await in useEffect, it will cause an infinite loop
       });
-  }, []); // empty array means this will only run once when the component mounts
+  }, [url]); // empty array means this will only run once when the component mounts
   console.log(trips);
 
   return (
     <div>
       <h1>Ready To Go?</h1>
-      <button>All</button>
-      <button>Select Location</button>
+      <button onClick={() => setUrl('http://localhost:3010/trips')}>All</button>
+      <button onClick={() => setUrl('http://localhost:3010/trips?location=Myanmar')}>Only Myanmar</button>
       <ul>
         {trips.map(trip => (
           <li key={trip.id}>
